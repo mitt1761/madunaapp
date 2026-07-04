cbf342cdc9aaec13773751d227db981857f6ef80
NurulHalisaz <nurulhalisa.z04@gmail.com>
2026-07-04 20:08:20 +0800

    Refactor orderModel.js methods for clarity
    Refactor create method for better readability and add order_date to findAll query.

--- back-end/models/orderModel.js
+++ back-end/models/orderModel.js
@@ -1,19 +1,121 @@
 const db = require('../config/database');
 
 const Order = {
+
     // Simpan pesanan baru
-    create: (user_id, quantity, total_price, callback) => {
-        const query = "INSERT INTO orders (user_id, quantity, total_price, status) VALUES (?, ?, ?, 'pending')";
-        db.query(query, [user_id, quantity, total_price], callback);
+    create: (
+        user_id,
+        quantity,
+        total_price,
+        callback
+    ) => {
+
+        const query =
+        `
+        INSERT INTO orders
+        (
+            user_id,
+            quantity,
+            total_price,
+            status
+        )
+        VALUES
+        (
+            ?,
+            ?,
+            ?,
+            'pending'
+        )
+        `;
+
+        db.query(
+            query,
+            [
+                user_id,
+                quantity,
+                total_price
+            ],
+            callback
+        );
+
     },
-    // Ambil semua data pesanan (untuk Admin)
+    createOrderItem: (
+    order_id,
+    product_id,
+    quantity,
+    callback
+) => {
+
+    db.query(
+        `
+        INSERT INTO order_items
+        (
+            order_id,
+            product_id,
+            quantity
+        )
+        VALUES
+        (
+            ?,
+            ?,
+            ?
+        )
+        `,
+        [
+            order_id,
+            product_id,
+            quantity
+        ],
+        callback
+    );
+
+},
+
+    // Ambil semua pesanan (Admin)
     findAll: (callback) => {
+
         const query = `
-            SELECT orders.id, users.name, orders.quantity, orders.total_price, orders.status 
-            FROM orders 
-            JOIN users ON orders.user_id = users.id`;
-        db.query(query, callback);
+            SELECT
+                orders.id,
+                users.name,
+                orders.quantity,
+                orders.total_price,
+                orders.status,
+                orders.order_date
+            FROM orders
+            JOIN users
+            ON orders.user_id = users.id
+            ORDER BY orders.id DESC
+        `;
+
+        db.query(
+            query,
+            callback
+        );
+
+    },
+
+    // Ambil riwayat pesanan customer
+    getHistory: (
+        userId,
+        callback
+    ) => {
+
+        const query = `
+            SELECT *
+            FROM orders
+            WHERE user_id = ?
+            ORDER BY id DESC
+        `;
+
+        db.query(
+            query,
+            [userId],
+            callback
+        );
+
     }
+
 };
 
 module.exports = Order;
