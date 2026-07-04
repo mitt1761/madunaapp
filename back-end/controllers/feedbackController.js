c5abd1ff7cb7f8c44339f402e20362f2341ef1e2
NurulHalisaz <nurulhalisa.z04@gmail.com>
2026-07-04 20:11:30 +0800

    Add getComments and deleteComment functions

--- back-end/controllers/feedbackController.js
+++ back-end/controllers/feedbackController.js
@@ -9,3 +9,51 @@ exports.submitComment = (req, res) => {
         res.status(201).json({ message: "Terima kasih atas masukannya!" });
     });
 };
+exports.getComments = (req,res)=>{
+
+    const query =
+    "SELECT * FROM comments ORDER BY id DESC";
+
+    db.query(
+        query,
+        (err,results)=>{
+
+            if(err){
+
+                return res.status(500).json({
+                    message:"Gagal mengambil komentar"
+                });
+
+            }
+
+            res.json(results);
+
+        }
+    );
+
+};
+exports.deleteComment = (req,res)=>{
+
+    const { id } = req.params;
+
+    db.query(
+        "DELETE FROM comments WHERE id=?",
+        [id],
+        (err,result)=>{
+
+            if(err){
+
+                return res.status(500).json({
+                    message:"Gagal menghapus komentar"
+                });
+
+            }
+
+            res.json({
+                message:"Komentar berhasil dihapus"
+            });
+
+        }
+    );
+
+};
