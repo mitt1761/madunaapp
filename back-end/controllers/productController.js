2e6a447ddecdbc0fb970234a3e3bf12f84ca5689
NurulHalisaz <nurulhalisa.z04@gmail.com>
2026-07-04 20:12:10 +0800

    Update productController.js

--- back-end/controllers/productController.js
+++ back-end/controllers/productController.js
@@ -1,9 +1,122 @@
 const Product = require('../models/productModel');
 
 exports.getProducts = (req, res) => {
-    // Memanggil fungsi dari Product Model
+
     Product.findAll((err, results) => {
-        if (err) return res.status(500).json({ message: "Gagal mengambil data" });
+
+        if (err) {
+            return res.status(500).json({
+                message: "Gagal mengambil data"
+            });
+        }
+
         res.status(200).json(results);
+
     });
+
+};
+
+exports.updateProduct = (req, res) => {
+
+    const { id } = req.params;
+
+    const {
+        name,
+        category,
+        volume,
+        description,
+        image,
+        price,
+        stock
+    } = req.body;
+
+    Product.updateProduct(
+        id,
+        name,
+        category,
+        volume,
+        description,
+        image,
+        price,
+        stock,
+        (err, result) => {
+
+            if (err) {
+
+                return res.status(500).json({
+                    message: "Gagal update produk"
+                });
+
+            }
+
+            res.json({
+                message: "Produk berhasil diperbarui"
+            });
+
+        }
+    );
+
+};
+exports.createProduct = (req,res)=>{
+
+    const {
+        name,
+        category,
+        volume,
+        description,
+        image,
+        price,
+        stock
+    } = req.body;
+
+    Product.createProduct(
+        name,
+        category,
+        volume,
+        description,
+        image,
+        price,
+        stock,
+        (err,result)=>{
+
+            if(err){
+
+                return res.status(500).json({
+                    message:"Gagal menambah produk"
+                });
+
+            }
+
+            res.status(201).json({
+                message:"Produk berhasil ditambahkan"
+            });
+
+        }
+    );
+
+};
+
+exports.deleteProduct = (req,res)=>{
+
+    const { id } = req.params;
+
+    Product.deleteProduct(
+        id,
+        (err,result)=>{
+
+            if(err){
+
+                return res.status(500).json({
+                    message:"Gagal menghapus produk"
+                });
+
+            }
+
+            res.json({
+                message:"Produk berhasil dihapus"
+            });
+
+        }
+    );
+
 };
